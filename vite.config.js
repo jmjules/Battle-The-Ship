d6cdb8da65918dec4aeac1081ede41c7aef2a0ab
jmjules <126357309+jmjules@users.noreply.github.com>
2023-11-08 22:55:05 +0100

    right repo name?

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
-  base: "/Battle-the-ship/",
+  base: "/Battle-The-Ship/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
